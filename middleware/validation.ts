6344c9e177eaf56a02671e1b3bf93f5ab9401622
Yotrages <qayyumoladimeji@gmail.com>
2025-11-05 23:05:44 +0100

    changed authLimiter

--- middleware/validation.ts
+++ middleware/validation.ts
@@ -96,7 +96,7 @@ export const generalLimiter = rateLimit({
 
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20, 
+  max: 100, 
   message: {
     success: false,
     message: 'Too many authentication attempts, please try again later.',
